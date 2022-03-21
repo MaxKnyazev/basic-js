afd5cf4d9daf69a38a59f47b3a877965729ce720
MaxKnyazev <max.knyazev77@gmail.com>
2022-03-21 19:21:17 +0300

    feat: complete dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let result = '';
+
+  members.forEach(elem => {
+    if (typeof(elem) === 'string' && elem !== '') {
+      result += elem.trim()[0]
+    }
+  })
+
+  return result
+          .toUpperCase()
+          .split('')
+          .sort()
+          .join('')
 }
 
 module.exports = {
