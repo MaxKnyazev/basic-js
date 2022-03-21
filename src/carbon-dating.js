1ad3ff9256d1135cf8d9733d90dcb93c37213b7c
MaxKnyazev <max.knyazev77@gmail.com>
2022-03-21 19:02:21 +0300

    feat: complete carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,21 +6,33 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
 function dateSample(sampleActivity) {
+  const allowedCharacters = '0123456789.';
+  const K = 0.693 / HALF_LIFE_PERIOD;
 
+  if (typeof sampleActivity !== 'string' || +sampleActivity === 0) return false;
+
+  for (let i = 0; i < sampleActivity.length; i++) {
+    if (!allowedCharacters.includes(sampleActivity[i])) return false;
+  }
+
+  if (+sampleActivity > MODERN_ACTIVITY) return false;
+
+  let t = Math.log(MODERN_ACTIVITY / sampleActivity) / K;
+  return Math.ceil(t);
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
