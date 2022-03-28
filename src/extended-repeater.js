e8da1a9d8cd9ba856a0f2733c7f961d43853ea81
MaxKnyazev <max.knyazev77@gmail.com>
2022-03-28 18:29:45 +0300

    feat: complete extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let {repeatTimes, separator, addition, additionRepeatTimes, additionSeparator} = options;
+  let result = '';
+  let resultAddition = '';
+
+  if (separator === undefined) separator = '+';
+  if (additionSeparator === undefined) additionSeparator = '|';
+  addition = addition !== undefined ? `${addition}` : '';
+  str = `${str}`;
+
+  if (additionRepeatTimes === undefined) {
+    resultAddition = addition;
+  } else {
+    for (let i = 1; i <= additionRepeatTimes; i++) {
+      resultAddition += i === additionRepeatTimes ? `${addition}` : `${addition}${additionSeparator}`;
+    }
+  }
+
+  if (repeatTimes === undefined) {
+    result = `${str}${resultAddition}`
+  } else {
+    for (let i = 1; i <= repeatTimes; i++) {
+      result += i === repeatTimes ? `${str}${resultAddition}` : `${str}${resultAddition}${separator}`;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
