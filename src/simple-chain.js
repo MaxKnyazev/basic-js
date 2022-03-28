7d367d1b5a94a8e4fa48e49a2ff90f871cfb174d
MaxKnyazev <max.knyazev77@gmail.com>
2022-03-28 20:10:09 +0300

    feat: complete simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  resultValues: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.resultValues.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.resultValues.push(String(value));
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (this.resultValues[position - 1] === undefined) {
+      this.resultValues = [];
+      throw new Error('You can\'t remove incorrect link!');
+    }
+
+    this.resultValues.splice(position - 1, 1)
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.resultValues.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let resultChain = this.resultValues.map(elem => `( ${elem} )`).join('~~');
+    this.resultValues = [];
+    return resultChain;
   }
 };
 
